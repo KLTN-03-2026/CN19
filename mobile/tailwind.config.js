87bb65f2374f934b3e1102aec807fa160dad3b9e
MinhPhuong7304 <tranminhphuong732004@gmail.com>
2026-04-13 15:18:44 +0700

    feat(mobile): Khởi tạo dự án Expo và cấu hình TailwindCSS

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        neon: {
+          green: '#39FF14',
+          brand: '#00F2FE'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
